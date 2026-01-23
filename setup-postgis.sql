c8f2238316182f904bbda902f714ef6983c5483f
kikokaraba <kikokaraba@gmail.com>
2026-01-24 00:18:07 +0100

    v

--- setup-postgis.sql
+++
@@ -1,7 +0,0 @@
--- PostGIS Extension Setup for Railway PostgreSQL
--- Spustite tento príkaz v Railway Query tab alebo cez psql
-
-CREATE EXTENSION IF NOT EXISTS postgis;
-
--- Overenie, že PostGIS funguje
-SELECT PostGIS_version();
