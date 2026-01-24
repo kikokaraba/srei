11b7ff79690ebb886c1fcdbc51fb5692ba7b602d
kikokaraba <kikokaraba@gmail.com>
2026-01-24 20:59:20 +0100

    fix: change map to light theme (OpenStreetMap)

--- components/dashboard/InteractiveMap.tsx
+++ components/dashboard/InteractiveMap.tsx
@@ -434,13 +434,13 @@ export default function InteractiveMap() {
           zoom={mapZoom}
           className="h-full w-full"
           zoomControl={false}
-          style={{ background: "#0f172a" }}
+          style={{ background: "#f8fafc" }}
         >
           <MapController center={mapCenter} zoom={mapZoom} />
           
-          {/* Dark tile layer */}
+          {/* Light tile layer - like realitymap.sk */}
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           />
 
